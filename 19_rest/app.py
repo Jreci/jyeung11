517027959168f41e62aa31b501193cd09188cee7
Jreci <>
2021-04-07 08:40:49 -0400

    Heading and edits

--- 19_rest/app.py
+++ 19_rest/app.py
@@ -1,16 +1,23 @@
+"""
+Team Cosmic Stardust- Victoria Gao, Jessica Yeung, Andrew Jiang
+SoftDev
+K19 -- A RESTful Journey Skyward
+2020-04-05
+"""
 from flask import Flask, render_template
-import urllib.request as urllib2, json
+import urllib.request, json
 app = Flask(__name__)
 
 @app.route("/")
 def root():
-    file = open("key_nasa.txt", "r")
-    key = "https://api.nasa.gov/planetary/apod?api_key=" + file.read()
-    u = urllib2.urlopen(key)
+    file = open("key_nasa.txt",'r')
+    key = "https://api.nasa.gov/planetary/apod?api_key="+file.read()
+    u = urllib.request.urlopen(key)
     response = u.read()
-    data = json.loads (response)
-    return render_template("main.html", pic = data['url'], text = data["explanation"])
+    data = json.loads(response)
+    return render_template("main.html", pic = data['url'], txt = data["explanation"])
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.debug = True
     app.run()
+    
