5ad22545d105bef8b84b1ed29c7388d4a2c35aa5
Jreci <>
2021-05-11 10:06:23 -0400

    k28 update

--- 28_propagate/3/events.js
+++ 28_propagate/3/events.js
@@ -8,7 +8,7 @@ var table = document.getElementsByTagName('table')[0];
 var clicky = function(e) {
   alert( this.innerHTML );
   //Q: What will happen when next line is uncommented?
-  //e.stopPropagation();
+  // e.stopPropagation();
 };
 
 for (var x=0; x < tds.length; x++) {
